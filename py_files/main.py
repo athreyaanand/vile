721929a892ecc4ed891f07a5ce2e493da91da333
josephcsoti <josephcsoti@gmail.com>
2018-06-23 01:43:56 -0700

    Make main run universe

--- py_files/main.py
+++ py_files/main.py
@@ -1,33 +1,18 @@
-#RUN THIS
-import sys
-sys.path.append('Individual')
-sys.path.append('Individual/Helpers')
+from Universe.Universe import Universe
 
-import random
+from Individual.Individual import Individual
+from Individual.DNA import DNA
 
-from DNA import DNA
-from Individual import Individual
-from BabyFactory import BabyFactory
-from MatchFactory import MatchFactory
+universe = Universe()
 
-# Create seed people
-dna_a = DNA("11110000")
-dna_b = DNA("00001111")
-adam = Individual(dna_a, "Eve", "GOD", "F", "GOD", "GOD", [])
-eve = Individual(dna_a, "Adam", "GOD", "M", "GOD", "GOD", [])
+adam = Individual(DNA("11110000"), "Eve", "GOD", "F", "GOD", "GOD", [])
+eve = Individual(DNA("00001111"), "Adam", "GOD", "M", "GOD", "GOD", [])
+seed = [adam, eve]
 
-print(adam.info())
-print(eve.info())
+universe.spawn(seed)
 
-Universe = [adam, eve]
+universe.run(10)
+universe.run(3)
+universe.run(2)
 
-BabyFactory = BabyFactory()
-MatchFactory = MatchFactory(Universe)
-
-for x in range(0,10):
-  matches = MatchFactory.find_matches()
-  for y in range(0, len(matches)-1):
-    #print("--- MATCHED: " + matches[y][0].info() + " & " + matches[y][1].info())
-    baby = BabyFactory.make(matches[y][0], matches[y][1])
-    Universe.append(baby)
-    MatchFactory.update_universe(Universe)
+universe.end()
