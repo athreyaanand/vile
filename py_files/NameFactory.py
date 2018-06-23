bb6178a1884aabdd918ce80ad990366590440240
josephcsoti <josephcsoti@gmail.com>
2018-06-22 20:44:57 -0700

    Reworked "name_generator.py" to be a class

---
+++ py_files/NameFactory.py
@@ -0,0 +1,24 @@
+import random
+
+class NameFactory(object):
+  def __init__(self):
+    self.vowels = ["a"] * 50 + ["e"] * 50 + ["i"] * 50 + ["o"] * 50 + ["u"] * 50 + ["y"] * 10 + ["oe"] * 15 + ["ee"] * 15 + ["oo"] * 15
+    self.consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z", "ph", "th", "ck", "ch", "ng", "tr", "sh"] 
+    self.lengths = ['3'] * 25 + ['4'] * 194 + ['5'] * 277 + ['6'] * 308 + ['7'] * 85 + ['8'] * 15 + ['9'] * 7 + ['10'] * 5
+
+  def generate_name(self, gender):
+    namelength = random.choice(self.lengths)
+    name = ""
+    for x in range (int(namelength)):
+      if x%2 == 0:
+        name += self.consonants[random.randint(0, len(self.consonants)-1)]
+      else:
+        name += self.vowels[random.randint(0, len(self.vowels)-1)]
+    
+    if str(gender) is "F" and random.randint(0, 100) < 90 and int(namelength)%2 != 1:
+      name += self.vowels[random.randint(0, len(self.vowels)-1)]
+    
+    if (str(gender) == "F") and (random.randint(0, 100) < 90):
+      name = name[1:]
+    
+    return name
