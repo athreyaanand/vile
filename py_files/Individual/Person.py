76b0dbdde42504a8fe6546f0a6827cc60c3c2350
josephcsoti <josephcsoti@gmail.com>
2018-06-22 23:21:30 -0700

    Removed unused person (Replaced by Individual)

--- py_files/Individual/Person.py
+++
@@ -1,9 +0,0 @@
-class Person(object):
-  def __init__(self, dna, name, sex):
-    self.dna = dna
-    self.name = name
-    self.sex = sex
-  def info(self):
-    return self.name + " (" + self.sex + ")"
-  def get_name(self):
-    return self.name
