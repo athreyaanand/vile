4f42aeb7a58c4de2411eb015fe23ab0f42bd0f01
josephcsoti <josephcsoti@gmail.com>
2018-06-25 23:54:50 -0700

    Testing Trait Factory

--- py_files/Factories/TraitFactory.py
+++ py_files/Factories/TraitFactory.py
@@ -23,20 +23,15 @@ class TraitFactory(object):
     color = hex(int(segment, 2))
     print(color)
     ff = self.hex_to_rgb(color)
-    print(ff)
-    aa = self.closest_colour(ff)
-    print(aa)
-    print(self.get_colour_name(ff))
+    print("val: " + str(ff))
+    result = self.get_colour_name(ff)
+
+    print(result)
+    return result
     #return self.get_colour_name(aa)
     #return self.hex_to_rgb(color)
     #return self.get_colour_name(self.closest_color(self.hex_to_rgb(color)))
 
-  def getNearestWebSafeColor(r, g, b):
-    r = int(round( ( r / 255.0 ) * 5 ) * 51)
-    g = int(round( ( g / 255.0 ) * 5 ) * 51)
-    b = int(round( ( b / 255.0 ) * 5 ) * 51)
-    return (r, g, b)
-  
   def hex_to_rgb(self, value):
     value = value.lstrip('0x')
     return tuple(int(value[i:i+1], 16) for i in range(0, 3, 1))
@@ -44,6 +39,7 @@ class TraitFactory(object):
   def closest_colour(self, requested_colour):
     min_colours = {}
     for key, name in webcolors.css3_hex_to_names.items():
+        print(key + " " + name)
         r_c, g_c, b_c = webcolors.hex_to_rgb(key)
         rd = (r_c - requested_colour[0]) ** 2
         gd = (g_c - requested_colour[1]) ** 2
@@ -55,6 +51,6 @@ class TraitFactory(object):
     try:
         closest_name = actual_name = webcolors.rgb_to_name(requested_colour)
     except ValueError:
-        closest_name = closest_colour(requested_colour)
+        closest_name = self.closest_colour(requested_colour)
         actual_name = None
-    return actual_name, closest_name
+    return closest_name
