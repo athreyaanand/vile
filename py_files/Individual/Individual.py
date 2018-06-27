5c1182f416e9c0ce88d417f481a39bd600ffb53e
Athreya Anand <31478366+athreyaanand@users.noreply.github.com>
2018-06-26 21:52:03 -0700

    Updated marry

--- py_files/Individual/Individual.py
+++ py_files/Individual/Individual.py
@@ -45,8 +45,11 @@ class Individual(object):
       partner.spouse = self
       if partner.gender == "F":
         partner.lastname = self.lastname
+        partner.name = patner.firstname + " " + partner.lastname
       else:
         self.lastname = partner.lastname
+        self.name = self.firstname + " " + self.lastname
+      
   
   def info(self):
     return self.name + " (" + self.gender + ")[" + self.dna.getDNA() + "]"
