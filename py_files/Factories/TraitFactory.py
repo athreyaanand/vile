3cbc5933aef5a447b768944607ae2d131020db0d
josephcsoti <josephcsoti@gmail.com>
2018-06-26 22:13:02 -0700

    Cleaned up code

--- py_files/Factories/TraitFactory.py
+++ py_files/Factories/TraitFactory.py
@@ -7,9 +7,6 @@ from Individual.DNA import DNA
 
 class TraitFactory(object):
 
-  def __init__(self):
-    self.zero = 0
-
   # def create_DNA(self, traits):
   #   return DNA("110010")
 
@@ -18,31 +15,18 @@ class TraitFactory(object):
     return self.calc_color()
   
   def calc_color(self):
-    segment = self.dna.getDNA() # pick a seg
-    #color = hex(int(segment, 2))
+    segment = self.dna.dna # pick a seg
     tbit = hex(int(segment, 2))[2:]
     color = str(tbit[0]) + str(tbit[0]) + str(tbit[1]) + str(tbit[1]) +str(tbit[2]) + str(tbit[2])
-    print(color)
-    ff = self.hex_to_rgb(color)
-    print("val: " + str(ff))
-    result = self.get_colour_name(ff)
-
-    print(result)
-    return result
-    #return self.get_colour_name(aa)
-    #return self.hex_to_rgb(color)
-    #return self.get_colour_name(self.closest_color(self.hex_to_rgb(color)))
+    return self.get_colour_name(self.hex_to_rgb(color))
 
   def hex_to_rgb(self, value):
     value = value.lstrip('0x')
-    print (len(value))
-    print (value) 
     return tuple(int(value[i:i+2], 16) for i in range(0, 6, 2))
     
   def closest_colour(self, requested_colour):
     min_colours = {}
     for key, name in webcolors.css3_hex_to_names.items():
-        #print(key + " " + name)
         r_c, g_c, b_c = webcolors.hex_to_rgb(key)
         rd = (r_c - requested_colour[0]) ** 2
         gd = (g_c - requested_colour[1]) ** 2
