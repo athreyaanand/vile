5feb0982ea39442c8bcecc281e4b8ef3ac5d301e
josephcsoti <josephcsoti@gmail.com>
2018-06-29 00:32:25 -0700

    Added methods to "calculate" traits from gene value

--- py_files/Factories/TraitFactory.py
+++ py_files/Factories/TraitFactory.py
@@ -1,43 +1,39 @@
-# Returns an array of traits, given DNA
-# OR creates DNA from given traits
-
-import webcolors
-
 from Individual.DNA import DNA
 
 class TraitFactory(object):
 
+  def __init__(self):
+    self.traits = ["Gender", "Height", "Weight", "Eye Color", "Skin Color"]
+
   # def create_DNA(self, traits):
   #   return DNA("110010")
 
-  def create_traits(self, Individual):
-    self.dna = Individual.dna
-    return self.calc_color()
+  def create_traits(self, DNA):
+    res = [None]*5
+    res[0] = self.calc_gender(DNA.get_gene("Gender"))
+    res[1] = self.calc_height(DNA.get_gene("Height"))
+    res[2] = self.calc_weight(DNA.get_gene("Weight"))
+    res[3] = self.calc_eyecolor(DNA.get_gene("Eye Color"))
+    res[4] = self.calc_skincolor(DNA.get_gene("Skin Color"))
+
+    trait_dict = {}
+
+    for i in range(0, len(self.traits)):
+      trait_dict[self.traits[i]] = res[i]
+
+    return trait_dict
+
+  def calc_gender(self, gene):
+    return gene[2:]
   
-  def calc_color(self):
-    segment = self.dna.dna # pick a seg
-    tbit = hex(int(segment, 2))[2:]
-    color = str(tbit[0]) + str(tbit[0]) + str(tbit[1]) + str(tbit[1]) +str(tbit[2]) + str(tbit[2])
-    return self.get_colour_name(self.hex_to_rgb(color))
+  def calc_height(self, gene):
+    return gene[2:]
 
-  def hex_to_rgb(self, value):
-    value = value.lstrip('0x')
-    return tuple(int(value[i:i+2], 16) for i in range(0, 6, 2))
-    
-  def closest_colour(self, requested_colour):
-    min_colours = {}
-    for key, name in webcolors.css3_hex_to_names.items():
-        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
-        rd = (r_c - requested_colour[0]) ** 2
-        gd = (g_c - requested_colour[1]) ** 2
-        bd = (b_c - requested_colour[2]) ** 2
-        min_colours[(rd + gd + bd)] = name
-    return min_colours[min(min_colours.keys())]
+  def calc_weight(self, gene):
+    return gene[2:]
 
-  def get_colour_name(self, requested_colour):
-    try:
-        closest_name = actual_name = webcolors.rgb_to_name(requested_colour)
-    except ValueError:
-        closest_name = self.closest_colour(requested_colour)
-        actual_name = None
-    return closest_name
+  def calc_eyecolor(self, gene):
+    return gene[2:]
+
+  def calc_skincolor(self, gene):
+    return gene[2:]
