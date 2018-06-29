df0162dd2c106990998a839b069081f0f1eb1616
josephcsoti <josephcsoti@gmail.com>
2018-06-29 00:33:13 -0700

    Changed tests to reflect previous changes

--- py_files/trait_test.py
+++ py_files/trait_test.py
@@ -1,11 +1,18 @@
 from Factories.TraitFactory import TraitFactory
 from Individual.DNA import DNA
-from Individual.Individual import Individual
 
 TraitFactory = TraitFactory()
 
-adam = Individual(DNA("010111011100"), "Eve", "GOD", "F", "GOD", "GOD", [])
+dnas = [None]*5
+dnas[0] = DNA("1-9923:2-3243:1-5323:3-6341:2-1203")
+dnas[1] = DNA("1-7293:2-6198:1-1102:3-2042:2-2212")
+dnas[2] = DNA("1-2938:2-0162:1-1820:3-6623:2-1204")
+dnas[3] = DNA("1-1792:2-3243:1-3384:3-4027:2-0411")
+dnas[4] = DNA("1-8002:2-5142:1-0032:3-9000:2-1922")
 
-res = TraitFactory.create_traits(adam)
-print("Color: " + res)
-
+for i in range(0, len(dnas)):
+  traits = TraitFactory.create_traits(dnas[i])
+  print("DNA " + str(i) + ": ", end="")
+  for key in traits:
+    print (key + ": " + traits[key], end=", ")
+  print()
